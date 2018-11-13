05673d2faa39d8726da917e8a9f00119098ec78f
Clang <chenhu1001@qq.com>
2018-11-13 09:55:57 +0800

    增加国内访问站点

--- upload.sh
+++ upload.sh
@@ -1,4 +1,6 @@
 hexo clean  
 hexo generate
+cp baidu_verify_QlXZgAkD3G.html public/baidu_verify_QlXZgAkD3G.html
+cp baidu_verify_kGgjmJoWEu.html public/baidu_verify_kGgjmJoWEu.html
 cp googlec2ea9934f0a4f099.html public/googlec2ea9934f0a4f099.html 
 hexo deploy
