85901069c069db348c0a20ea24782e68552771a0
Clang <chenhu1001@qq.com>
2019-02-02 09:22:08 +0800

    modify

--- upload.sh
+++ upload.sh
@@ -1,6 +1,7 @@
-hexo clean  
+#!/usr/bin/env bash
+hexo clean
 hexo generate
 cp baidu_verify_QlXZgAkD3G.html public/baidu_verify_QlXZgAkD3G.html
 cp baidu_verify_kGgjmJoWEu.html public/baidu_verify_kGgjmJoWEu.html
-cp googlec2ea9934f0a4f099.html public/googlec2ea9934f0a4f099.html 
+cp googlec2ea9934f0a4f099.html public/googlec2ea9934f0a4f099.html
 hexo deploy
