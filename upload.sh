7e54502f4633c49095b0d3b42eb263eb8307726c
Clang <chenhu1001@qq.com>
2019-02-11 15:26:50 +0800

    腾讯统计、添加打赏、添加robots、添加百度文件验证

--- upload.sh
+++ upload.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 hexo clean
 hexo generate
-cp baidu_verify_QlXZgAkD3G.html public/baidu_verify_QlXZgAkD3G.html
-cp baidu_verify_kGgjmJoWEu.html public/baidu_verify_kGgjmJoWEu.html
+cp baidu_verify_pPAplH5qPr.html public/baidu_verify_pPAplH5qPr.html
 cp googlec2ea9934f0a4f099.html public/googlec2ea9934f0a4f099.html
 cp sitemap.xml public/sitemap.xml
 cp baidusitemap.xml public/baidusitemap.xml
