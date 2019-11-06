2ea042914d6d1e7c21322585eda8704992432f00
Clang <chenhu1001@qq.com>
2019-11-06 19:48:50 +0800

    站长验证文件

--- upload.sh
+++ upload.sh
@@ -2,7 +2,7 @@
 hexo clean
 hexo generate
 cp baidu_verify_pPAplH5qPr.html public/baidu_verify_pPAplH5qPr.html
-cp googlec2ea9934f0a4f099.html public/googlec2ea9934f0a4f099.html
+cp google8819a7729474aec5.html public/google8819a7729474aec5.html
 cp sitemap.xml public/sitemap.xml
 cp baidusitemap.xml public/baidusitemap.xml
 hexo deploy
