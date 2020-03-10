fc6ebbb51d666ab66d334938e3f988607a9e0ba6
Clang <chenhu1001@qq.com>
2020-03-10 16:12:06 +0800

    增加直播文件

--- upload.sh
+++ upload.sh
@@ -7,4 +7,5 @@ cp baidu_verify_pPAplH5qPr.html public/baidu_verify_pPAplH5qPr.html
 cp google8819a7729474aec5.html public/google8819a7729474aec5.html
 cp sitemap.xml public/sitemap.xml
 cp baidusitemap.xml public/baidusitemap.xml
+cp list.m3u public/list.m3u
 hexo deploy
